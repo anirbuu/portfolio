3676072f3474917e7a9ab62d7ac73c954d232fa5
Anirban <anirbanbakshi5@gmail.com>
2026-05-27 23:09:20 +0530

    Fix Gmail SMTP on Render

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -25,12 +25,14 @@ const sendContactMessage = async (req, res) => {
 
     // EMAIL TRANSPORT
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-      }
-    });
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
 
     // SEND EMAIL
     await transporter.sendMail({
